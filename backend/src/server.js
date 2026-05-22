088edaf81e2a6203f06e034ae6f48c0ca2a451b6
AshmitSherigar <sherigarashmit@gmail.com>
2026-05-22 16:00:05 +0530

    new routes to fetch info

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,17 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.join(__dirname, '../.env') });
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
-import express from 'express';
-import connectDB from './db/db.js';
-import protectedRoutes from './routes/auth.protected.js';
+import express from "express";
+import connectDB from "./db/db.js";
+import protectedRoutes from "./routes/auth.protected.js";
 // import cookieParser from 'cookie-parser';
-import { router as userRoute } from './routes/auth.route.js';
-import cors from 'cors';
+import { router as userRoute } from "./routes/auth.route.js";
+import { router as typingRoute } from "./routes/typing.route.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -19,8 +20,9 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 // app.use(cookieParser());
-app.use('/api/auth', userRoute);
-app.use('/api/protected', protectedRoutes);
+app.use("/api/auth", userRoute);
+app.use("/api/protected", protectedRoutes);
+app.use("/api/typing", typingRoute);
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
